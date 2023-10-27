f498b6c77ac47e1892f5a48f20af1a91da682a03
lucasnevespereira <pereiraneveslucas@gmail.com>
2023-10-27 09:08:42 +0200

    kinde auth

--- components/Header.vue
+++ components/Header.vue
@@ -1,30 +1,35 @@
 <script setup lang="ts">
-
 </script>
 
 <template>
-    <div class="navbar bg-base-100 sticky shadow-sm">
+    <div class="navbar bg-base-100 sticky shadow-sm p-4">
         <div class="flex-1">
             <a class="btn btn-ghost hover:bg-none normal-case text-xl">menuAI</a>
         </div>
-        <div class="flex-none">
-            <div class="dropdown dropdown-end">
+        <div class="flex-none mr-2">
+            <div v-if="$auth.loggedIn" class="dropdown dropdown-end">
                 <label tabindex="0" class="btn btn-ghost btn-circle avatar">
                     <div class="w-10 rounded-full">
-                        <img src="https://ui-avatars.com/api/?name=Lucas+Pereira" />
+                        <img :src="$auth.user.picture"/>
                     </div>
                 </label>
-                <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                <ul tabindex="0"
+                    class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                     <li>
                         <a class="justify-between">
                             Profile
-                            <span class="badge">New</span>
                         </a>
                     </li>
-                    <li><a>Settings</a></li>
-                    <li><a>Logout</a></li>
+                    <li>
+                        <NuxtLink to="/api/logout" external>
+                            Logout
+                        </NuxtLink>
+                    </li>
                 </ul>
             </div>
+            <NuxtLink v-else to="/api/login" external>
+                Sign in
+            </NuxtLink>
         </div>
     </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
     // devtools: { enabled: true },
     modules: [
         '@nuxtjs/tailwindcss',
-        "nuxt-phosphor-icons"
+        "nuxt-phosphor-icons",
+        '@nuxtjs/kinde'
     ],
 })
